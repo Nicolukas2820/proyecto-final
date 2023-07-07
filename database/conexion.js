af079b06250e458ab3f7e3e38d6d5c9c94fd71c3
Miguel Angel <migg.ro3@gmail.com>
2023-07-07 13:07:58 -0500

    Adaptación de veteriana a hospital

--- database/conexion.js
+++ database/conexion.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
-  database: 'veterinaria'
+  password: 'sj*bRc$FRS5jGC',
+  database: 'hospital'
 });
 
 // Conexión a la base de datos
